72778d26b667b8eaa0f1eaadab632761d3347f55
kako <kauanribeirobravo@gmail.com>
2023-02-17 17:30:43 -0300

    erros e script2

---
+++ script2.js
@@ -0,0 +1,7 @@
+// tipagem dinamica
+
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true;
+
+// js é uma linguagem multiparadigma, significa que da para resolver o mesmo problema de multiplas formas
